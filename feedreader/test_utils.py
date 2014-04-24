c8780bc3864dc329d429a105ad7e53a183f6c06b
Paul Ahern <ahernp@ahernp.com>
2014-04-24 12:05:44 +0100

    Tidied feedparser util unit tests

--- feedreader/test_utils.py
+++ feedreader/test_utils.py
@@ -14,9 +14,9 @@ import pytz
 
 
 @patch('feedreader.utils.feedparser.parse')
-class TestPollFeed(TestCase):
+class TestPollFeedBozoException(TestCase):
     """
-    Test polling feeds.
+    Test polling feeds where Bozo Exception returned.
     """
     def setUp(self):
         feed_mock = Mock(spec=Feed)
@@ -27,63 +27,82 @@ class TestPollFeed(TestCase):
     def test_bozo_exception(self, parse_mock):
         """Test with Bozo Exception returned"""
         parse_mock.return_value.feed.bozo_exception = 'bozo_exception returned'
-        with patch('sys.stdout', new=StringIO()) as fake_out:  # Suppress printed output from test
-            poll_feed(self.feed_mock, verbose=True)
-
-    def test_published_time(self, parse_mock):
-        """Test Published Time variations"""
-        del parse_mock.return_value.feed.bozo_exception
-        parse_mock.return_value.feed.published_parsed = (2014, 01, 01,
-                                                         12, 0, 0,
-                                                         2, 1, 0)  # 2014-01-01 12:00:00
-
-        # No published time in DB
-        feed_mock = self.feed_mock
-        feed_mock.published_time = None
-        with patch('sys.stdout', new=StringIO()) as fake_out:  # Suppress printed output from test
-            poll_feed(feed_mock, verbose=True)
-
-        # Published time in DB later than on feed
-        feed_mock.published_time = pytz.utc.localize(datetime(2014, 01, 01, 13, 0, 0))
-        with patch('sys.stdout', new=StringIO()) as fake_out:  # Suppress printed output from test
-            poll_feed(feed_mock, verbose=True)
-
-    def test_missing_attribute(self, parse_mock):
-        """Test with missing attribute: description_detail"""
-        del parse_mock.return_value.feed.bozo_exception
-        parse_mock.return_value.feed.published_parsed = (2014, 01, 01,
-                                                         12, 0, 0,
-                                                         2, 1, 0)  # 2014-01-01 12:00:00
-        del parse_mock.return_value.feed.description_detail
-        with patch('sys.stdout', new=StringIO()) as fake_out:  # Suppress printed output from test
-            poll_feed(self.feed_mock, verbose=True)
-
-    def test_with_feed_description(self, parse_mock):
-        """Test with missing attribute: description_detail"""
-        del parse_mock.return_value.feed.bozo_exception
-        parse_mock.return_value.feed.published_parsed = (2014, 01, 01,
-                                                         12, 0, 0,
-                                                         2, 1, 0)  # 2014-01-01 12:00:00
-        parse_mock.return_value.feed.description_detail.type = 'text/plain'
-        parse_mock.return_value.feed.description = 'Test Feed Description'
-        with patch('sys.stdout', new=StringIO()) as fake_out:  # Suppress printed output from test
+        with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
             poll_feed(self.feed_mock, verbose=True)
 
 
-@patch('feedreader.utils.feedparser.parse')
-class TestPollEntries(TestCase):
+class TestPollFeed(TestCase):
+    """
+    Test polling feeds.
+    """
     def setUp(self):
+        # Create feedparser.parse_mock object
+        parse_mock = Mock()
+        del parse_mock.return_value.feed.bozo_exception
+        parse_mock.return_value.feed.published_parsed = (2014, 01, 01,
+                                                         12, 0, 0,
+                                                         2, 1, 0)  # 2014-01-01 12:00:00
+        parse_mock.return_value.entries = []
+        self.parse_mock = parse_mock
+
+        # Create .models.Feed mock object
         feed_mock = Mock(spec=Feed)
         feed_mock.xml_url = 'test-feed-url'
         feed_mock.published_time = None
         self.feed_mock = feed_mock
 
-    def test_feed_entry_blank_title(self, parse_mock):
+    def test_published_time(self):
+        """Test Published Time variations"""
+        with patch('feedreader.utils.feedparser.parse', self.parse_mock):
+            # No published time in DB
+            feed_mock = self.feed_mock
+            feed_mock.published_time = None
+            with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
+                poll_feed(feed_mock, verbose=True)
+
+            # Published time in DB later than on feed
+            feed_mock.published_time = pytz.utc.localize(datetime(2014, 01, 01, 13, 0, 0))
+            with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
+                poll_feed(feed_mock, verbose=True)
+
+    def test_missing_attribute(self):
         """Test with missing attribute: description_detail"""
+        parse_mock = self.parse_mock
+        del parse_mock.return_value.feed.description_detail
+        with patch('feedreader.utils.feedparser.parse', parse_mock):
+            with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
+                poll_feed(self.feed_mock, verbose=True)
+
+    def test_with_feed_description(self):
+        """Test with description_detail present"""
+        parse_mock = self.parse_mock
+        parse_mock.return_value.feed.description_detail.type = 'text/plain'
+        parse_mock.return_value.feed.description = 'Test Feed Description'
+        with patch('feedreader.utils.feedparser.parse', parse_mock):
+            with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
+                poll_feed(self.feed_mock, verbose=True)
+
+
+class TestPollEntries(TestCase):
+    def setUp(self):
+        # Create feedparser.parse_mock object
+        parse_mock = Mock()
         del parse_mock.return_value.feed.bozo_exception
         parse_mock.return_value.feed.published_parsed = (2014, 01, 01,
                                                          12, 0, 0,
                                                          2, 1, 0)  # 2014-01-01 12:00:00
+        self.parse_mock = parse_mock
+
+        # Create .models.Feed mock object
+        feed_mock = Mock(spec=Feed)
+        feed_mock.xml_url = 'test-feed-url'
+        feed_mock.published_time = None
+        self.feed_mock = feed_mock
+
+
+    def test_feed_entry_blank_title(self):
+        """Test with missing attribute: description_detail"""
+        parse_mock = self.parse_mock
         entry_attrs = {'link': 'test_entry_link',
                        'published_parsed': (2014, 01, 01, 12, 0, 0, 2, 1, 0),  # 2014-01-01 12:00:00
                        }
@@ -92,16 +111,14 @@ class TestPollEntries(TestCase):
         parse_mock.return_value.entries = [entry_mock]
         db_entry_mock = Mock()
         db_entry_mock.objects.get_or_create.return_value = (Mock(), True)
-        with patch('feedreader.utils.Entry', db_entry_mock):
-            with patch('sys.stdout', new=StringIO()) as fake_out:  # Suppress printed output from test
-                poll_feed(self.feed_mock, verbose=True)
+        with patch('feedreader.utils.feedparser.parse', parse_mock):
+            with patch('feedreader.utils.Entry', db_entry_mock):
+                with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
+                    poll_feed(self.feed_mock, verbose=True)
 
-    def test_feed_entry_missing_description(self, parse_mock):
+    def test_feed_entry_missing_description(self):
         """Test with missing attribute: description_detail"""
-        del parse_mock.return_value.feed.bozo_exception
-        parse_mock.return_value.feed.published_parsed = (2014, 01, 01,
-                                                         12, 0, 0,
-                                                         2, 1, 0)  # 2014-01-01 12:00:00
+        parse_mock = self.parse_mock
         entry_attrs = {'link': 'test_entry_link',
                        'published_parsed': (2014, 01, 01, 12, 0, 0, 2, 1, 0),  # 2014-01-01 12:00:00
                        }
@@ -110,16 +127,14 @@ class TestPollEntries(TestCase):
         parse_mock.return_value.entries = [entry_mock]
         db_entry_mock = Mock()
         db_entry_mock.objects.get_or_create.return_value = (Mock(), True)
-        with patch('feedreader.utils.Entry', db_entry_mock):
-            with patch('sys.stdout', new=StringIO()) as fake_out:  # Suppress printed output from test
-                poll_feed(self.feed_mock, verbose=True)
+        with patch('feedreader.utils.feedparser.parse', parse_mock):
+            with patch('feedreader.utils.Entry', db_entry_mock):
+                with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
+                    poll_feed(self.feed_mock, verbose=True)
 
-    def test_feed_entry_future_published_time(self, parse_mock):
-        """Test with missing attribute: description_detail"""
-        del parse_mock.return_value.feed.bozo_exception
-        parse_mock.return_value.feed.published_parsed = (2014, 01, 01,
-                                                         12, 0, 0,
-                                                         2, 1, 0)  # 2014-01-01 12:00:00
+    def test_feed_entry_future_published_time(self):
+        """Test with future entry published time"""
+        parse_mock = self.parse_mock
         entry_attrs = {'link': 'test_entry_link',
                        'published_parsed': (2114, 01, 01, 12, 0, 0, 2, 1, 0),  # 2114-01-01 12:00:00
                        }
@@ -129,7 +144,8 @@ class TestPollEntries(TestCase):
         parse_mock.return_value.entries = [entry_mock]
         db_entry_mock = Mock()
         db_entry_mock.objects.get_or_create.return_value = (Mock(), True)
-        with patch('feedreader.utils.Entry', db_entry_mock):
-            with patch('sys.stdout', new=StringIO()) as fake_out:  # Suppress printed output from test
-                poll_feed(self.feed_mock, verbose=True)
+        with patch('feedreader.utils.feedparser.parse', parse_mock):
+            with patch('feedreader.utils.Entry', db_entry_mock):
+                with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
+                    poll_feed(self.feed_mock, verbose=True)
 
