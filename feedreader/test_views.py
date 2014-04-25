247b0215783119d261d76e5e015bd6865df05a06
Paul Ahern <ahernp@ahernp.com>
2014-04-25 12:50:10 +0100

    100% unit test coverage

--- feedreader/test_views.py
+++ feedreader/test_views.py
@@ -4,12 +4,27 @@ from __future__ import absolute_import
 from StringIO import StringIO
 
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.test.client import Client
 
 from .models import Entry
 
 from mock import Mock, patch
+
+OPML = """<?xml version="1.0" ?>
+<opml version="2.0">
+  <head>
+    <title>Feedreader Feeds</title>
+  </head>
+  <body>
+    <outline text="ahernp.com blog" type="rss" xmlUrl="http://ahernp.com/blog/feed"/>
+    <outline text="News">
+      <outline text="Reuters: Most Read Articles" type="rss" xmlUrl="http://feeds.reuters.com/reuters/MostRead?format=xml"/>
+    </outline>
+  </body>
+</opml>
+"""
 
 
 class MarkEntryReadTest(TestCase):
@@ -48,3 +63,23 @@ class MarkEntryReadTest(TestCase):
                              200,
                              'URL %s: Unexpected status code, got %s expected 200' %
                              (url, response.status_code))
+
+
+class LoadOPMLTest(TestCase):
+    """Load OPML file"""
+    def setUp(self):
+        self.user = get_user_model().objects.create_user('john', 'john@montypython.com', 'password')
+        self.user.is_staff = True
+        self.user.save()
+        self.client = Client()
+        self.client.login(username='john', password='password')
+        self.opml_file = SimpleUploadedFile("feedreader.opml", OPML)
+
+    def test_loading_opml_file(self):
+        """Load OPML file"""
+        url = '/feedreader/import_opml/'
+        response = self.client.post(url, {'opml_file': self.opml_file})
+        self.assertEqual(response.status_code,
+                         302,
+                         'URL %s: Unexpected status code, got %s expected 302' %
+                            (url, response.status_code))
