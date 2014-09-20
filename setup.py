a1dd365ffdf8ce37506096985a71f253df6c8923
Paul Ahern <ahernp@ahernp.com>
2014-09-20 11:10:30 +0100

    Upgraded to Django 1.7.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        'Django==1.6.2',
+        'Django==1.7',
         'django-braces==1.4.0',
         'factory-boy==2.3.1',
         'feedparser==5.1.3',
