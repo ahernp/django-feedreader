5eede374394f1d05ef7dbd3398745a3ee747d99e
Paul Ahern <ahernp@ahernp.com>
2015-01-17 08:12:26 +0000

    Revert "Converted some tests to py.test."
    This reverts commit 6099b45b1b31cdffd5c91a48be0e046e6ab802b9.

--- feedreader/test_views.py
+++ feedreader/test_views.py
@@ -1,4 +1,4 @@
-"""Feedreader Utils Unit Test. Designed for py.test"""
+"""Feedreader Utils Unit Test."""
 from __future__ import absolute_import
 
 from StringIO import StringIO
@@ -8,7 +8,6 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.test.client import Client
 
-from .factories import GroupFactory, FeedFactory, EntryFactory
 from .forms import AddFeedsForm
 from .models import Entry, Feed, Group
 from .simple_test_server import (PORT, setUpModule as server_setup,
@@ -17,70 +16,108 @@ from .views import EditFeeds, UpdateItem
 
 from mock import Mock, patch
 
-import pytest
-
 OPML = """<?xml version="1.0" ?>
 <opml version="2.0">
   <head>
     <title>Feedreader Feeds</title>
   </head>
   <body>
-    <outline text="ahernp.com blog" type="rss" xmlUrl="https://ahernp.com/blog/feed"/>
+    <outline text="ahernp.com blog" type="rss" xmlUrl="http://ahernp.com/blog/feed"/>
     <outline text="News">
-      <outline text="ahernp.com blog" type="rss" xmlUrl="https://ahernp.com/blog/feed"/>
+      <outline text="Reuters: Most Read Articles" type="rss" xmlUrl="http://feeds.reuters.com/reuters/MostRead?format=xml"/>
     </outline>
   </body>
 </opml>
 """
 
-def test_mark_unread_entry(admin_client):
-    """Mark unread entry as read"""
-    entry = EntryFactory.create()
-    entry.read_flag = False
-    entry.save()
 
-    url = '/feedreader/mark_entry_read/?entry_id=%s' % (entry.id)
-    response = admin_client.get(url, secure=True)
-    assert response.status_code == 200
+class MarkEntryReadTest(TestCase):
+    """
+    Test MarkEntryRead view
+    """
+    def setUp(self):
+        self.user = get_user_model().objects.create_user('john', 'john@montypython.com', 'password')
+        self.user.is_staff = True
+        self.user.save()
+        self.client = Client()
+        self.client.login(username='john', password='password')
 
-def test_mark_missing_entry(admin_client):
-    """Mark unread entry as read"""
-    entry_get_mock = Mock()
-    entry_get_mock.side_effect = Entry.DoesNotExist
-    with patch('feedreader.views.Entry.objects.get', entry_get_mock):
-        url = '/feedreader/mark_entry_read/?entry_id=1'
-        response = admin_client.get(url, secure=True)
-        assert response.status_code == 200
+    def test_mark_unread_entry(self):
+        """Mark unread entry as read"""
+        entry_class_mock = Mock(spec=Entry)
+        entry_object_mock = Mock()
+        entry_object_mock.read_flag = False
+        entry_class_mock.objects.get.return_value = entry_object_mock
+        with patch('feedreader.views.Entry', entry_class_mock):
+            url = '/feedreader/mark_entry_read/?entry_id=1'
+            response = self.client.get(url, secure=True)
+            self.assertEqual(response.status_code,
+                             200,
+                             'URL %s: Unexpected status code, got %s expected 200' %
+                             (url, response.status_code))
 
-def test_loading_opml_file(admin_client):
+    def test_mark_missing_entry(self):
+        """Mark unread entry as read"""
+        entry_get_mock = Mock()
+        entry_get_mock.side_effect = Entry.DoesNotExist
+        with patch('feedreader.views.Entry.objects.get', entry_get_mock):
+            url = '/feedreader/mark_entry_read/?entry_id=1'
+            response = self.client.get(url, secure=True)
+            self.assertEqual(response.status_code,
+                             200,
+                             'URL %s: Unexpected status code, got %s expected 200' %
+                             (url, response.status_code))
+
+
+class LoadOPMLTest(TestCase):
     """Load OPML file"""
-    opml_file = SimpleUploadedFile("feedreader.opml", OPML)
-    url = '/feedreader/edit_feeds/'
-    response = admin_client.post(url,
-                                 {'opml_file': opml_file},
-                                 secure=True)
-    assert response.status_code == 200
+    def setUp(self):
+        self.user = get_user_model().objects.create_user('john', 'john@montypython.com', 'password')
+        self.user.is_staff = True
+        self.user.save()
+        self.client = Client()
+        self.client.login(username='john', password='password')
+        self.opml_file = SimpleUploadedFile("feedreader.opml", OPML)
 
-@pytest.mark.django_db
-def test_add_new_feed():
-    """Create new feed and group"""
-    def dict_lookup(*args):
-        returns = {'feed_url': 'test_url',
-                   'feed_group': 'test_group',
-                   'new_group': 'test_new_group',
-                   'opml_file': None}
-        return returns[args[0]]
-    form_mock = Mock()
-    form_mock.cleaned_data.get.side_effect = dict_lookup
-    feed_class_mock = Mock(spec=Feed)
-    request_mock = Mock()
+    def test_loading_opml_file(self):
+        """Load OPML file"""
+        url = '/feedreader/edit_feeds/'
+        response = self.client.post(url, 
+                                    {'opml_file': self.opml_file},
+                                    secure=True)
+        self.assertEqual(response.status_code,
+                         200,
+                         'URL %s: Unexpected status code, got %s expected 200' %
+                            (url, response.status_code))
 
-    with patch('feedreader.views.AddFeedsForm', form_mock):
-        with patch('feedreader.views.Feed', feed_class_mock):
-            edit_feeds_view = EditFeeds()
-            edit_feeds_view.request = request_mock
-            response = edit_feeds_view.form_valid(form_mock)
-            assert response.status_code == 200
+
+class EditFeedsTest(TestCase):
+    """
+    Test EditFeeds view
+    """
+    def setUp(self):
+        def dict_lookup(*args):
+            returns = {'feed_url': 'test_url',
+                       'feed_group': 'test_group',
+                       'new_group': 'test_new_group',
+                       'opml_file': None}
+            return returns[args[0]]
+        self.form_mock = Mock()
+        self.form_mock.cleaned_data.get.side_effect = dict_lookup
+        self.feed_class_mock = Mock(spec=Feed)
+        self.request_mock = Mock()
+
+    def test_add_new_feed(self):
+        """Mark create new feed and group"""
+        with patch('feedreader.views.AddFeedsForm', self.form_mock):
+            with patch('feedreader.views.Feed', self.feed_class_mock):
+                edit_feeds_view = EditFeeds()
+                edit_feeds_view.request = self.request_mock
+                response = edit_feeds_view.form_valid(self.form_mock)
+                self.assertEqual(response.status_code,
+                                 200,
+                                 'Unexpected status code, got %s expected 200' %
+                                 (response.status_code))
 
 def setUpModule():
     server_setup()
@@ -88,62 +125,83 @@ def setUpModule():
 def tearDownModule():
     server_teardown()
 
-@pytest.fixture(scope='module')
-def group_on_db(db):
-    """Populate Database"""
-    group = Group.objects.create(name='Test Group')
-    return group
+class UpdateItemTest(TestCase):
+    """
+    Test UpdateItem view
+    """
+    def setUp(self):
+        """Create data and login"""
+        self.feed = Feed.objects.create(xml_url='http://localhost:%s/test/feed' % (PORT))
+        self.group = Group.objects.create(name='Test Group')
+        self.feed.group = self.group
+        self.feed.save()
 
-@pytest.fixture(scope='module')
-def feed_on_db(db, group_on_db):
-    """Populate Database"""
-    feed = Feed.objects.create(xml_url='http://localhost:%s/test/feed' % (PORT))
-    feed.group = group_on_db
-    feed.save()
-    return feed
+        self.user = get_user_model().objects.create_user('john',
+                                                         'john@montypython.com',
+                                                         'password')
+        self.user.is_staff = True
+        self.user.save()
+        self.client = Client()
+        self.client.login(username='john', password='password')
 
-def test_delete_item(admin_client, feed_on_db):
-    """Delete item"""
-    url = '/feedreader/update/'
-    response = admin_client.post(url,
-                                 {'identifier': 'feedreader-Feed-delete-%s' % feed_on_db.id,
-                                     'data_value': 'on'},
-                                 secure=True)
-    assert response.status_code == 200
+    def test_delete_item(self):
+        """Delete item"""
+        url = '/feedreader/update/'
+        response = self.client.post(url, 
+                                    {'identifier': 'feedreader-Feed-delete-%s' % self.feed.id,
+                                        'data_value': 'on'},
+                                    secure=True)
+        self.assertEqual(response.status_code,
+                         200,
+                         'Unexpected status code, got %s expected 200' %
+                         (response.status_code))
 
-def test_update_text(admin_client, feed_on_db):
-    """Update text field"""
-    url = '/feedreader/update/'
-    response = admin_client.post(url,
-                                 {'identifier': 'feedreader-Feed-title-%s' % feed_on_db.id,
-                                     'data_value': 'Test Title 2'},
-                                 secure=True)
-    assert response.status_code == 200
+    def test_update_text(self):
+        """Update text field"""
+        url = '/feedreader/update/'
+        response = self.client.post(url, 
+                                    {'identifier': 'feedreader-Feed-title-%s' % self.feed.id,
+                                        'data_value': 'Test Title 2'},
+                                    secure=True)
+        self.assertEqual(response.status_code,
+                         200,
+                         'Unexpected status code, got %s expected 200' %
+                         (response.status_code))
+        feed = Feed.objects.get(pk=self.feed.id)
+        self.assertEqual(feed.title,
+                         'Test Title 2',
+                         'Unexpected feed name: Got "%s" expected "Test Title 2"' %
+                         (feed.title))
 
-    feed = Feed.objects.get(pk=feed_on_db.id)
-    assert feed.title == 'Test Title 2'
 
-def test_update_boolean(admin_client, admin_user):
-    """Update boolean field"""
-    url = '/feedreader/update/'
-    response = admin_client.post(url,
-                                 {'identifier': 'auth-User-is_superuser-%s' % admin_user.id,
-                                  'data_value': 'true'},
-                                 secure=True)
-    assert response.status_code == 200
+    def test_update_boolean(self):
+        """Update boolean field"""
+        url = '/feedreader/update/'
+        response = self.client.post(url, 
+                                    {'identifier': 'auth-User-is_superuser-%s' % self.user.id, 'data_value': 'true'},
+                                    secure=True)
+        self.assertEqual(response.status_code,
+                         200,
+                         'Unexpected status code, got %s expected 200' %
+                         (response.status_code))
 
-def test_update_foreignkey(admin_client, feed_on_db, group_on_db):
-    """Update foreign key"""
-    url = '/feedreader/update/'
-    response = admin_client.post(url,
-                                 {'identifier': 'feedreader-Feed-group-%s' % feed_on_db.id,
-                                      'data_value': group_on_db.id},
-                                 secure=True)
-    assert response.status_code == 200
-
-    # Update foreign key to None
-    response = admin_client.post(url,
-                                 {'identifier': 'feedreader-Feed-group-%s' % feed_on_db.id,
-                                      'data_value': ''},
-                                 secure=True)
-    assert response.status_code == 200
+    def test_update_foreignkey(self):
+        """Update foreign key"""
+        url = '/feedreader/update/'
+        response = self.client.post(url, 
+                                    {'identifier': 'feedreader-Feed-group-%s' % self.feed.id,
+                                         'data_value': self.group.id},
+                                    secure=True)
+        self.assertEqual(response.status_code,
+                         200,
+                         'Unexpected status code, got %s expected 200' %
+                         (response.status_code))
+        # Update foreign key to None
+        response = self.client.post(url, 
+                                    {'identifier': 'feedreader-Feed-group-%s' % self.feed.id,
+                                         'data_value': ''},
+                                    secure=True)
+        self.assertEqual(response.status_code,
+                         200,
+                         'Unexpected status code, got %s expected 200' %
+                         (response.status_code))
--- feedreader/tests.py
+++ feedreader/tests.py
@@ -1,4 +1,4 @@
-"""Feedreader Unit Test. Designed for py.test"""
+"""Feedreader Unit Test."""
 from __future__ import absolute_import
 
 import SimpleHTTPServer
@@ -7,18 +7,21 @@ import SocketServer
 import threading
 
 from django.core.management import call_command
+from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth import get_user_model
 
 from .factories import GroupFactory, FeedFactory, EntryFactory
 from .models import Options
 from .simple_test_server import (PORT, setUpModule as server_setup,
                                  tearDownModule as server_teardown)
 
-import pytest
+from mock import patch
 
 TEST_URLS = [
     # (url, status_code, text_on_page)
     ('/feedreader/', 200, 'Feed Reader'),
-    ('/feedreader/num_unread/', 200, '"unread_total": 5'),
+    ('/feedreader/num_unread/', 200, 'unread_feed'),
     # Poll Feed
     ('/feedreader/entry_list/?feed_id=1&poll_flag=1', 200, '<div id="entry_id='),
     # Poll Group
@@ -62,40 +65,65 @@ def tearDownModule():
     server_teardown()
 
 
-def test_urls(admin_client):
-    """Visit each URL in turn"""
-    entry = EntryFactory.create()
-    group = GroupFactory.create()
-    feed = FeedFactory.create()
-    feed.xml_url = 'http://localhost:%s/test/feed' % PORT
-    feed.group = group
-    feed.save()
+class WorkingURLsTest(TestCase):
+    """
+    Visit various URLs in Feedreader to ensure they are working.
+    """
+    def setUp(self):
+        """Create data and login"""
+        entry = EntryFactory.create()
+        group = GroupFactory.create()
+        feed = FeedFactory.create()
+        feed.xml_url = 'http://localhost:%s/test/feed' % PORT
+        feed.group = group
+        feed.save()
 
-    for url, status_code, expected_text in TEST_URLS:
-        response = admin_client.get(url, secure=True)
-        assert response.status_code == status_code
-        if response.status_code == 200 and expected_text:
-            assert expected_text in response.content
+        self.user = get_user_model().objects.create_user('john', 'john@montypython.com', 'password')
+        self.user.is_staff = True
+        self.user.save()
+        self.client = Client()
+        self.client.login(username='john', password='password')
 
-@pytest.mark.django_db
-def test_poll_feeds():
-    """Test poll_feeds command."""
-    entry = EntryFactory.create()
-    group = GroupFactory.create()
-    feed = FeedFactory.create()
-    feed.xml_url = 'http://localhost:%s/test/feed' % PORT
-    feed.group = group
-    feed.save()
+    def test_urls(self):
+        """Visit each URL in turn"""
+        for url, status_code, expected_text in TEST_URLS:
+            response = self.client.get(url, secure=True)
+            self.assertEqual(response.status_code,
+                             status_code,
+                             'URL %s: Unexpected status code, got %s expected %s' %
+                                (url, response.status_code, status_code))
+            if response.status_code == 200 and expected_text:
+                self.assertContains(response,
+                                    expected_text,
+                                    msg_prefix='URL %s' % (url))
 
-    args = []
-    opts = {'verbose': True}
 
-    # Ensure some Entries are deleted
-    feedreader_options = Options.manager.get_options()
-    feedreader_options.max_entries_saved = 1
-    feedreader_options.save()
-    call_command('poll_feeds', *args, **opts)
+class TestPollFeedsCommand(TestCase):
+    """
+    Test the command which polls the feeds.
+    """
+    def setUp(self):
+        """Create data"""
+        entry = EntryFactory.create()
+        group = GroupFactory.create()
+        feed = FeedFactory.create()
+        feed.xml_url = 'http://localhost:%s/test/feed' % PORT
+        feed.group = group
+        feed.save()
 
-    # Default Options created if none are found
-    Options.objects.all().delete()
-    call_command('poll_feeds', *args, **opts)
+    def test_poll_feeds(self):
+        """Test poll_feeds command."""
+        args = []
+        opts = {'verbose': True}
+
+        # Ensure some Entries are deleted
+        feedreader_options = Options.manager.get_options()
+        feedreader_options.max_entries_saved = 1
+        feedreader_options.save()
+        with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
+            call_command('poll_feeds', *args, **opts)
+
+        # Default Options created if none are found
+        Options.objects.all().delete()
+        with patch('sys.stdout', new=StringIO()):  # Suppress printed output from test
+            call_command('poll_feeds', *args, **opts)
